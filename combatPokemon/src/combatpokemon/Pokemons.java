abe8eef3402e5d28c33ce0ef55fd4b0684bbdd56
Dario <dgracid@gmail.com>
2023-02-14 23:07:48 +0100

    Update Pokemons.java

--- combatPokemon/src/combatpokemon/Pokemons.java
+++ combatPokemon/src/combatpokemon/Pokemons.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package combatpokemon;
-
+import java.util.*;
 /**
  *
  * @author dagr5755
@@ -29,5 +29,13 @@ public class Pokemons {
     public String toString() {
         String frase = "Nom Pokemon " + this.nomPokemon + " y " + " tiene " + this.CP + " puntos  ";
         return frase;
+    }
+    
+    
+    public void atac (){
+        Random rd = new Random();
+        int atac = rd.nextInt(0, 10);
+        System.out.println("El " + this.nomPokemon + "ataca amb la força " + atac);
+        ;
     }
 }
